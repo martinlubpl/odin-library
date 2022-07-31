276182b434460fcbf0d7052d8013192c3f2a1862
Marcin Majewski <smdesign.eu@gmail.com>
2022-07-31 17:23:34 +0200

    book cards

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ let book3 = new Book('asdf', 'qwer', 1500, false);
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
+//end
 
-function Book(title, author, pages, comment, read) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -20,8 +21,46 @@ function addBookToLibrary(book) {
   myLibrary.unshift(book);
 }
 
-function displayBooks (arr) {
-    
+function displayBooks(arr) {
+  for (let book of arr) {
+    addCard(book);
+  }
+}
+
+function addCard(book) {
+  const main = document.querySelector('main');
+  const card = document.createElement('div');
+
+  
+  
+  const title = document.createElement('div');
+  title.innerText = book.title;
+  card.appendChild(title);
+  const author = document.createElement('div');
+  author.innerText = book.author;
+  card.appendChild(author);
+  const pages = document.createElement('div');
+  pages.innerText = book.pages;
+  card.appendChild(pages);
+
+  console.log(book.read);
+  const read = document.createElement('div');
+  const read_txt = (book.read) ? "Yes" : "No"
+  read.innerText = "Read: " + read_txt;
+  card.appendChild(read);
+
+  const statusBtn = document.createElement('button');
+  statusBtn.innerText = 'Change status';
+  card.appendChild(statusBtn);
+  
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerText = 'Delete from library';
+  card.appendChild(deleteBtn);
+
+
+  // append whole card
+  card.classList.add('card');
+  main.appendChild(card);
 }
 
 
@@ -29,4 +68,5 @@ function displayBooks (arr) {
 
 
 
-displayBooks()
+
+displayBooks(myLibrary);
